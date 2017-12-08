752493e21be44b5c9906b43369fc9080e9551cbb
own3mall <own3mall@gmail.com>
2017-12-07 17:08:28 -0700

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Swedish(Sweden)/modules/dsi.php
+++ lang/Swedish(Sweden)/modules/dsi.php
@@ -22,9 +22,9 @@
  *
  */
 
-define('dsi_long', "Dynamic Server Image");
-define('dsi_select_server', "Please choose a server from the drop-down list");
-define('dsi_admin_long', "Dynamic Server Image Settings");
-define('dsi_list', "Dynamic Server Image List");
-define('no_game_homes_assigned', "No game servers assigned for you. You need to ask your OGP admin to assign games for you.");
+define('OGP_LANG_dsi_long', "Dynamisk serverbild");
+define('OGP_LANG_dsi_select_server', "Välj en server från rullgardinsmenyn ");
+define('OGP_LANG_dsi_admin_long', "Dynamiska serverns bildinställningar");
+define('OGP_LANG_dsi_list', "Dynamisk serverbilds lista");
+define('OGP_LANG_no_game_homes_assigned', "Inga spelservrar är tilldelade till dig. Du måste fråga din OGP-administratör om att tilldela spel till dig.");
 ?>
