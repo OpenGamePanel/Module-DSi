b563df7962897c85836f0937cdcd0c1c3f384dc9
own3mall <own3mall@gmail.com>
2018-03-17 15:28:30 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Czech(CzechRepublic)/modules/dsi.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2017 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('OGP_LANG_dsi_long', "Obrázek dynamického serveru");
+define('OGP_LANG_dsi_select_server', "Z rozevíracího seznamu vyberte server");
+define('OGP_LANG_dsi_admin_long', "Dynamické nastavení obrazu serveru");
+define('OGP_LANG_dsi_list', "Seznam dynamických serverů");
+define('OGP_LANG_no_game_homes_assigned', "Nebyly přiděleny žádné herní servery. Potřebujete požádat administrátora OGP o přidělení her pro vás.");
+?>
