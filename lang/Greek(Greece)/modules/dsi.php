2040a7d9a101b538f40778f7fe63ac87eba2a423
own3mall <own3mall@gmail.com>
2018-05-28 02:38:20 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Greek(Greece)/modules/dsi.php
+++ lang/Greek(Greece)/modules/dsi.php
@@ -23,7 +23,7 @@
  */
 
 define('OGP_LANG_dsi_long', "Δυναμική Εικόνα Διακομιστή");
-define('OGP_LANG_dsi_select_server', "Παρακαλώ επιλέξετε έναν διακομιστή από την λίστα.");
+define('OGP_LANG_dsi_select_server', "Παρακαλώ επιλέξτε έναν διακομιστή από την λίστα.");
 define('OGP_LANG_dsi_admin_long', "Ρυθμίσεις Δυναμικής Εικόνας Διακομιστή");
 define('OGP_LANG_dsi_list', "Λίστα Δυναμικής Εικόνας Διακομιστή");
 define('OGP_LANG_no_game_homes_assigned', "Δεν έχετε διακομιστές ανατεθημένους στον λογαριασμό σας.");
