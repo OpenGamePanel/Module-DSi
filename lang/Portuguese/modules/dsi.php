a74d616fa0102f1ca81873200a1f0a952f496a91
own3mall <own3mall@gmail.com>
2018-06-03 07:35:29 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Portuguese/modules/dsi.php
+++ lang/Portuguese/modules/dsi.php
@@ -26,5 +26,5 @@ define('OGP_LANG_dsi_long', "Imagem dinâmica de servidores");
 define('OGP_LANG_dsi_select_server', "Escolha um servidor na lista suspensa");
 define('OGP_LANG_dsi_admin_long', "Definições de imagem dinâmica dos servidores");
 define('OGP_LANG_dsi_list', "Lista do servidor dinâmico de Imagem");
-define('OGP_LANG_no_game_homes_assigned', "You don't have any servers assigned to your account.");
+define('OGP_LANG_no_game_homes_assigned', "Você não tem nenhum servidor atribuído à sua conta.");
 ?>
