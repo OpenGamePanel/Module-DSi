24c810d760faf296dc8d1ac18bbbfd9c4a06e3ba
own3mall <own3mall@gmail.com>
2018-06-10 08:59:56 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Serbian(Serbia)/modules/dsi.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2018 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('OGP_LANG_dsi_long', "Динамичка слика сервера");
+define('OGP_LANG_dsi_select_server', "Молимо, одаберите сервер из падајуће листе");
+define('OGP_LANG_dsi_admin_long', "Подешавања динамичке слике сервера");
+define('OGP_LANG_dsi_list', "Листа динамичких слика сервера");
+define('OGP_LANG_no_game_homes_assigned', "Ви немате сервере повезаних са вашим налогом.");
+?>
