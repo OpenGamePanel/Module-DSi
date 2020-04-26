2602301d89e8bbb2a6909d9d50782cd56bb2efc4
own3mall <own3mall@gmail.com>
2020-04-26 14:34:36 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Hebrew(Israel)/modules/dsi.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2018 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('OGP_LANG_dsi_long', "Dynamic Server Image");
+define('OGP_LANG_dsi_select_server', "Please choose a server from the drop-down list");
+define('OGP_LANG_dsi_admin_long', "Dynamic Server Image Settings");
+define('OGP_LANG_dsi_list', "Dynamic Server Image List");
+define('OGP_LANG_no_game_homes_assigned', "You don't have any servers assigned to your account.");
+?>
