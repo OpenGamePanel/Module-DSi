b046c2148fad2b5535afc22c2fd7a8687056e9ae
own3mall <own3mall@gmail.com>
2021-02-12 23:31:05 -0700

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Czech(CzechRepublic)/modules/dsi.php
+++ lang/Czech(CzechRepublic)/modules/dsi.php
@@ -26,5 +26,5 @@ define('OGP_LANG_dsi_long', "Obrázek dynamického serveru");
 define('OGP_LANG_dsi_select_server', "Z rozevíracího seznamu vyberte server");
 define('OGP_LANG_dsi_admin_long', "Dynamické nastavení obrazu serveru");
 define('OGP_LANG_dsi_list', "Seznam dynamických serverů");
-define('OGP_LANG_no_game_homes_assigned', "You don't have any servers assigned to your account.");
+define('OGP_LANG_no_game_homes_assigned', "K vašemu účtu nejsou přiřazeny žádné servery.");
 ?>
